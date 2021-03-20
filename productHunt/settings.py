0403715f030d013cc98f8a7ad6bd4324e7ae6b85
Atif Ahmed <atif.ahmed9211@gmail.com>
2021-03-20 20:17:50 +0500

    products and accounts app added

--- productHunt/settings.py
+++ productHunt/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'productHunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'producthunt',
+        'USER': 'postgres',
+        'PASSWORD':'03350401095king',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
